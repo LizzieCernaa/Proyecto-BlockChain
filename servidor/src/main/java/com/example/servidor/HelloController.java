627a4b78b36c267e6f714af70913a51a26159b5b
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2023-11-20 11:56:10 -0600

    Agregando Frm

--- servidor/src/main/java/com/example/servidor/HelloController.java
+++ servidor/src/main/java/com/example/servidor/HelloController.java
@@ -1,12 +1,26 @@
 package com.example.servidor;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+ private Cifrado oCifradro;
 
+    @FXML
+    private void initialize() {
+       this.oCifradro= new Cifrado("NADA123456668");
+       String NodeData;
+       String pnodeData;
+       this.currentNode= pnodeData;
+       this.startServer();
+       this.startServer();
+
+       t
+
+    }
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
